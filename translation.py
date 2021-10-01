0d5634bbbdd1716c399456ad9405cb5e267472f0
Vedant VN <81044922+Vedant-VN@users.noreply.github.com>
2021-10-01 15:50:43 +0530

    OwO

--- translation.py
+++ translation.py
@@ -6,7 +6,7 @@ class Translation(object):
 
     BANNED_USER_TEXT = "Sad My Owner Has Banned You From Using Me. That Means You Can't Run Me Now! \n \n Contact : @NarutoRobot_Support To Know More..." 
     DOWNLOAD_START = "<b>Please Wait,I'm Downloading Your File To My server! I Hope We find Eren</b>"
-    UPLOAD_START = "<b>Downloading Is Now Completed,Okay I'm Uploading Your File into Our PM 😉</b>"
+    UPLOAD_START = "<b>Downloading Is Now Completed,Okay I'm Uploading Your File into Your PM 😏</b>"
     AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me, Support Our Channel @AnimeSigma❤️</b>"
     SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /delthumb ❤"
     DEL_ETED_CUSTOM_THUMB_NAIL = "Ok 🙂...thumbnail cleared succesfully ✌ Mistakes Happen its fine"
@@ -16,7 +16,7 @@ class Translation(object):
     CUSTOM_CAPTION_UL_FILE = " "
     NO_THUMB_FOUND = "Set Thumbnail or Bring Eren back? choose which is Easier"
     IFLONG_FILE_NAME = """Hey Piece of Shit...Decrease The Number Of Letters, 😐"""
-    ABOUT_ME = """<b>Hmm \nI'm Ackerman Mikasa 👋🏻 Just Another Telegram File Renamer Bot Which Can Rename Telegram Files. \nWell,I Support Permanent Thumbnail Feature.\n \ni can also convert files into videos \nStill Got Any Issues With Me 😔 Contact @NarutoRobot_Support & Tag @Vedant_Vn there For Your Answers Also please Bring eren Back </b>"""
+    ABOUT_ME = """<b>Hmm \nI'm Ackerman Mikasa 👋🏻 Just Another Telegram File Renamer Bot Which Can Rename Telegram Files. \nWell, I Support Permanent Thumbnail Feature.\n \ni can also convert files into videos \nStill Got Any Issues With Me 😔 Contact @NarutoRobot_Support & Tag @Vedant_Vn there For Your Answers Also please Bring eren Back </b>"""
     HELP_USER = """Checkout The Available Commands Here \n\nMake Sure You Join @AnimeSigma If You Find Me Useful 💞"""
     RENAME_HELP = """Here are The Available Commands In Rename \n\n\n▪️ <code>/rename</code> : Reply To An File/video With <code>/rename Filename.extension</code> For Renaming"""
     C2V_HELP = """Here Are The Available Commands In File To Video \n\n\n ▪️<code>/c2v</code> : Reply To An File With /c2v To Convert It Into Video"""
